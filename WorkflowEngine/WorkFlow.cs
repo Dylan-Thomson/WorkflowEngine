57f5861aa35d94befed077fd10b58f2bf05fc812
Dylan Thomson <djthomson88@gmail.com>
2019-05-30 13:01:23 -0400

    Create Workflow class that can execute a list of objects implementing the IActivity interface.

---
+++ WorkflowEngine/WorkFlow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace WorkflowEngine
+{
+    public class Workflow
+    {
+        private readonly IList<IActivity> _activities;
+
+        public Workflow()
+        {
+            _activities = new List<IActivity>();
+        }
+
+        public void RegisterActivity(IActivity activity)
+        {
+            _activities.Add(activity);
+        }
+
+        public void Run()
+        {
+            foreach (var activity in _activities)
+            {
+                activity.Execute();
+            }
+        }
+
+    }
+}
